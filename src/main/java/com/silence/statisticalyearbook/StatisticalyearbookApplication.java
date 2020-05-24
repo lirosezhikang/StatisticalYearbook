ae3f571a7f078724b2a601d81a7db2f64cbcd1a2
lirosezhikang <2118656521@qq.com>
2020-05-24 09:39:10 +0800

    first commit

---
+++ src/main/java/com/silence/statisticalyearbook/StatisticalyearbookApplication.java
@@ -0,0 +1,13 @@
+package com.silence.statisticalyearbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatisticalyearbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatisticalyearbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/silence/statisticalyearbook/StatisticalyearbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silence.statisticalyearbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatisticalyearbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
