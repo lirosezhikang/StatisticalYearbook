788e217264b5659c9f2ee46331541794a697e359
lirosezhikang <2118656521@qq.com>
2020-06-11 22:40:32 +0800

    数据库待完成

--- src/main/java/com/silence/statisticalyearbook/controller/GetDataController.java
+++ src/main/java/com/silence/statisticalyearbook/controller/GetDataController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
@@ -20,9 +21,39 @@ public class GetDataController {
 
     @GetMapping("/getData")
     @ResponseBody
-    public List<Echarts> getData(@RequestParam(name = "dataSelected",defaultValue = "total_value") String dataSelected,
+    public List<Object> getData(@RequestParam(name = "dataSelected",defaultValue = "total_value") String dataSelected,
                                  @RequestParam(name = "yearSelected",defaultValue = "y2019") String yearSelected,
                                  Model model){
+
+        AreaData maxAreaData =areaMapper.maxQuery(dataSelected, yearSelected);
+        AreaData minAreaData =areaMapper.minQuery(dataSelected, yearSelected);
+
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("total_value","国民生产总值");
+        map.put("population","人口");
+        map.put("per_value","人均生产总值");
+        map.put("salary","人均工资");
+        map.put("dis_income","人均可支配收入");
+        map.put("consumption","人均消费支出");
+        map.put("total_school","高校数");
+        map.put("edu_fee","教育经费");
+
+        String title=map.get(dataSelected);
+
+        HashMap<String, String> map2 = new HashMap<String, String>();
+        map2.put("total_value","(亿元)");
+        map2.put("population","(万人)");
+        map2.put("per_value","(元)");
+        map2.put("salary","(元)");
+        map2.put("dis_income","(元)");
+        map2.put("consumption","(元)");
+        map2.put("total_school","(所)");
+        map2.put("edu_fee","(万元)");
+        String unit=map2.get(dataSelected);
+
+        List<Object> result=new ArrayList<>();
+
+
         List<AreaData> areaDataList= areaMapper.areaQuery(dataSelected, yearSelected);
         List<Echarts> echartsList=new ArrayList<>();
 
@@ -35,6 +66,13 @@ public class GetDataController {
 
 
         model.addAttribute("areaDataList",areaDataList);
-        return echartsList;
+
+        result.add(0,echartsList);
+        result.add(1,maxAreaData);
+        result.add(2,minAreaData);
+        result.add(3,title);
+        result.add(4,unit);
+
+        return result;
     }
 }
--- src/main/java/com/silence/statisticalyearbook/controller/GetDataTopController.java
+++ src/main/java/com/silence/statisticalyearbook/controller/GetDataTopController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
@@ -21,9 +22,35 @@ public class GetDataTopController {
 
     @GetMapping("/getDataTop")
     @ResponseBody
-    public List<Echarts> getData(@RequestParam(name = "dataSelected",defaultValue = "total_value") String dataSelected,
+    public List<Object> getData(@RequestParam(name = "dataSelected",defaultValue = "total_value") String dataSelected,
                                  @RequestParam(name = "yearSelected",defaultValue = "y2019") String yearSelected,
                                  Model model){
+
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("total_value","国民生产总值");
+        map.put("population","人口");
+        map.put("per_value","人均生产总值");
+        map.put("salary","人均工资");
+        map.put("dis_income","人均可支配收入");
+        map.put("consumption","人均消费支出");
+        map.put("total_school","高校数");
+        map.put("edu_fee","教育经费");
+
+        String title=map.get(dataSelected);
+
+        HashMap<String, String> map2 = new HashMap<String, String>();
+        map2.put("total_value","(亿元)");
+        map2.put("population","(万人)");
+        map2.put("per_value","(元)");
+        map2.put("salary","(元)");
+        map2.put("dis_income","(元)");
+        map2.put("consumption","(元)");
+        map2.put("total_school","(所)");
+        map2.put("edu_fee","(万元)");
+        String unit=map2.get(dataSelected);
+
+        List<Object> result=new ArrayList<>();
+
         List<AreaData> areaDataList= areaMapper.areaQueryTop(dataSelected, yearSelected);
         List<Echarts> echartsList=new ArrayList<>();
 
@@ -36,6 +63,11 @@ public class GetDataTopController {
 
 
         model.addAttribute("areaDataList",areaDataList);
-        return echartsList;
+
+        result.add(0,echartsList);
+        result.add(1,title);
+        result.add(2,unit);
+
+        return result;
     }
 }
