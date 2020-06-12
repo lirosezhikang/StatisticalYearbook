fd2dff765243d1ac88c038cf00dc3db54ddf9757
lirosezhikang <2118656521@qq.com>
2020-06-12 22:45:40 +0800

    完成数据库的增删改查

--- src/main/java/com/silence/statisticalyearbook/controller/ManageController.java
+++ src/main/java/com/silence/statisticalyearbook/controller/ManageController.java
@@ -5,11 +5,10 @@ import com.silence.statisticalyearbook.mapper.ManageMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class ManageController {
@@ -30,5 +29,98 @@ public class ManageController {
     public List<TableAll> tableData(@RequestParam(name = "tableSelected",defaultValue = "total_value1") String tableName){
         List<TableAll> result=manageMapper.queryAll(tableName);
         return result;
+    }
+
+    @GetMapping("/save")
+    @ResponseBody
+    public void save(@RequestParam(name = "tableName") String tableName,
+                     @RequestParam(name = "year") String year,
+                     @RequestParam(name = "beijing",defaultValue = "0") double beijing,
+                     @RequestParam(name = "tianjing",defaultValue = "0") double tianjing,
+                     @RequestParam(name = "hebei",defaultValue = "0") double hebei,
+                     @RequestParam(name = "shanxi",defaultValue = "0") double shanxi,
+                     @RequestParam(name = "neimeng",defaultValue = "0") double neimeng,
+                     @RequestParam(name = "liaoning",defaultValue = "0") double liaoning,
+                     @RequestParam(name = "jilin",defaultValue = "0") double jilin,
+                     @RequestParam(name = "heilongjiang",defaultValue = "0") double heilongjiang,
+                     @RequestParam(name = "shanghai",defaultValue = "0") double shanghai,
+                     @RequestParam(name = "jiangsu",defaultValue = "0") double jiangsu,
+                     @RequestParam(name = "zhejiang",defaultValue = "0") double zhejiang,
+                     @RequestParam(name = "anhui",defaultValue = "0") double anhui,
+                     @RequestParam(name = "fujian",defaultValue = "0") double fujian,
+                     @RequestParam(name = "jiangxi",defaultValue = "0") double jiangxi,
+                     @RequestParam(name = "shandong",defaultValue = "0") double shandong,
+                     @RequestParam(name = "henan",defaultValue = "0") double henan,
+                     @RequestParam(name = "hubei",defaultValue = "0") double hubei,
+                     @RequestParam(name = "hunan",defaultValue = "0") double hunan,
+                     @RequestParam(name = "guangdong",defaultValue = "0") double guangdong,
+                     @RequestParam(name = "guangxi",defaultValue = "0") double guangxi,
+                     @RequestParam(name = "hainan",defaultValue = "0") double hainan,
+                     @RequestParam(name = "chongqin",defaultValue = "0") double chongqin,
+                     @RequestParam(name = "sichuan",defaultValue = "0") double sichuan,
+                     @RequestParam(name = "guizhou",defaultValue = "0") double guizhou,
+                     @RequestParam(name = "yunnan",defaultValue = "0") double yunnan,
+                     @RequestParam(name = "xizang",defaultValue = "0") double xizang,
+                     @RequestParam(name = "shaanxi",defaultValue = "0") double shaanxi,
+                     @RequestParam(name = "gansu",defaultValue = "0") double gansu,
+                     @RequestParam(name = "qinhai",defaultValue = "0") double qinhai,
+                     @RequestParam(name = "ningxia",defaultValue = "0") double ningxia,
+                     @RequestParam(name = "xinjiang",defaultValue = "0") double xinjiang
+                     ){
+
+        TableAll tableAll=new TableAll();
+        tableAll.setYear(year);
+        tableAll.setBeijing(beijing);
+        tableAll.setTianjing(tianjing);
+        tableAll.setHebei(hebei);
+        tableAll.setShanxi(shanxi);
+        tableAll.setNeimeng(neimeng);
+        tableAll.setLiaoning(liaoning);
+        tableAll.setJilin(jilin);
+        tableAll.setHeilongjiang(heilongjiang);
+        tableAll.setShanghai(shanghai);
+        tableAll.setJiangsu(jiangsu);
+        tableAll.setZhejiang(zhejiang);
+        tableAll.setAnhui(anhui);
+        tableAll.setFujian(fujian);
+        tableAll.setJiangxi(jiangxi);
+        tableAll.setShandong(shandong);
+        tableAll.setHenan(henan);
+        tableAll.setHubei(hubei);
+        tableAll.setHunan(hunan);
+        tableAll.setGuangdong(guangdong);
+        tableAll.setGuangxi(guangxi);
+        tableAll.setHainan(hainan);
+        tableAll.setChongqin(chongqin);
+        tableAll.setSichuan(sichuan);
+        tableAll.setGuizhou(guizhou);
+        tableAll.setYunnan(yunnan);
+        tableAll.setXizang(xizang);
+        tableAll.setShaanxi(shaanxi);
+        tableAll.setGansu(gansu);
+        tableAll.setQinhai(qinhai);
+        tableAll.setNingxia(ningxia);
+        tableAll.setXinjiang(xinjiang);
+
+        Integer flag=manageMapper.hasData(year,tableName);
+        if(flag>0){
+            manageMapper.updateRow(tableAll,tableName);
+        }else {
+            manageMapper.addRow(tableAll,tableName);
+        }
+    }
+
+    @GetMapping("/delete")
+    @ResponseBody
+    public Integer delete(@RequestParam(name = "tableName") String tableName,
+                       @RequestParam(name = "year") String year){
+
+        Integer flag=manageMapper.hasData(year,tableName);
+        if(flag>0){
+            manageMapper.delete(year,tableName);
+            return 1;
+        }else {
+            return 0;
+        }
     }
 }
--- src/main/java/com/silence/statisticalyearbook/mapper/ManageMapper.java
+++ src/main/java/com/silence/statisticalyearbook/mapper/ManageMapper.java
@@ -1,9 +1,7 @@
 package com.silence.statisticalyearbook.mapper;
 
 import com.silence.statisticalyearbook.dao.TableAll;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -13,8 +11,15 @@ public interface ManageMapper {
     @Select("SELECT * FROM ${tableName}")
     List<TableAll> queryAll(String tableName);
 
-    @Insert("INSERT INTO ${tableName}(\n" +
-            "\tyear, beijing, tianjing, hebei, shanxi, neimeng, liaoning, jilin, heilongjiang, shanghai, jiangsu, zhejiang, anhui, fujian, jiangxi, shandong, henan, hubei, hunan, guangdong, guangxi, hainan, chongqin, sichuan, guizhou, yunnan, xizang, shaanxi, gansu, qinhai, ningxia, xinjiang)\n" +
-            "\tVALUES (#{year}, #{beijing}, #{tianjing}, #{hebei}, #{shanxi}, #{neimeng}, #{liaoning}, #{jilin}, #{heilongjiang}, #{shanghai}, #{jiangsu}, #{zhejiang}, #{anhui}, #{fujian}, #{jiangxi}, #{shandong}, #{henan}, #{hubei}, #{hunan}, #{guangdong}, #{guangxi}, #{hainan}, #{chongqin}, #{sichuan}, #{guizhou}, #{yunnan}, #{xizang}, #{shaanxi}, #{gansu}, #{qinhai}, #{ningxia}, #{xinjiang})")
+    @Insert("INSERT INTO ${tableName}(year, beijing, tianjing, hebei, shanxi, neimeng, liaoning, jilin, heilongjiang, shanghai, jiangsu, zhejiang, anhui, fujian, jiangxi, shandong, henan, hubei, hunan, guangdong, guangxi, hainan, chongqin, sichuan, guizhou, yunnan, xizang, shaanxi, gansu, qinhai, ningxia, xinjiang) VALUES (#{tableAll.year}, #{tableAll.beijing}, #{tableAll.tianjing}, #{tableAll.hebei}, #{tableAll.shanxi}, #{tableAll.neimeng}, #{tableAll.liaoning}, #{tableAll.jilin}, #{tableAll.heilongjiang}, #{tableAll.shanghai}, #{tableAll.jiangsu}, #{tableAll.zhejiang}, #{tableAll.anhui}, #{tableAll.fujian}, #{tableAll.jiangxi}, #{tableAll.shandong}, #{tableAll.henan}, #{tableAll.hubei}, #{tableAll.hunan}, #{tableAll.guangdong}, #{tableAll.guangxi}, #{tableAll.hainan}, #{tableAll.chongqin}, #{tableAll.sichuan}, #{tableAll.guizhou}, #{tableAll.yunnan}, #{tableAll.xizang}, #{tableAll.shaanxi}, #{tableAll.gansu}, #{tableAll.qinhai}, #{tableAll.ningxia}, #{tableAll.xinjiang})")
     void addRow(TableAll tableAll,String tableName);
+
+    @Select("SELECT COUNT(*) FROM ${tableName} WHERE year = #{year}")
+    Integer hasData(String year,String tableName);
+
+    @Update("UPDATE ${tableName} SET beijing=#{tableAll.beijing}, tianjing=#{tableAll.tianjing}, hebei=#{tableAll.hebei}, shanxi=#{tableAll.shanxi}, neimeng=#{tableAll.neimeng}, liaoning=#{tableAll.liaoning}, jilin=#{tableAll.jilin}, heilongjiang=#{tableAll.heilongjiang}, shanghai=#{tableAll.shanghai}, jiangsu=#{tableAll.jiangsu}, zhejiang=#{tableAll.zhejiang}, anhui=#{tableAll.anhui}, fujian=#{tableAll.fujian}, jiangxi=#{tableAll.jiangxi}, shandong=#{tableAll.shandong}, henan=#{tableAll.henan}, hubei=#{tableAll.hubei}, hunan=#{tableAll.hunan}, guangdong=#{tableAll.guangdong}, guangxi=#{tableAll.guangxi}, hainan=#{tableAll.hainan}, chongqin=#{tableAll.chongqin}, sichuan=#{tableAll.sichuan}, guizhou=#{tableAll.guizhou}, yunnan=#{tableAll.yunnan}, xizang=#{tableAll.xizang}, shaanxi=#{tableAll.shaanxi}, gansu=#{tableAll.gansu}, qinhai=#{tableAll.qinhai}, ningxia=#{tableAll.ningxia}, xinjiang=#{tableAll.xinjiang} WHERE year = #{tableAll.year}")
+    void updateRow(TableAll tableAll,String tableName);
+
+    @Delete("DELETE FROM ${tableName} WHERE year = #{year}")
+    void delete(String year,String tableName);
 }
